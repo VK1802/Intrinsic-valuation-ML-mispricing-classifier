d92d22306659099d2478b989ea3946f7f52ff4d2
VK1802 <vaishnavikoppolu02@gmail.com>
2026-03-16 19:32:43 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
 import pandas as pd
+import pickle
+
+# Load trained model
+model = pickle.load(open("models/final_model.pkl","rb"))
 
 st.title("Stock Mispricing Classifier")
 
@@ -12,10 +16,13 @@ inflation = st.number_input("Inflation Rate")
 unemployment = st.number_input("Unemployment Rate")
 
 if st.button("Predict"):
+    
+    data = pd.DataFrame([[roa, debt_ratio, profit_margin, inflation, unemployment]],
+                        columns=["roa","debt_ratio","profit_margin","inflation","unemployment"])
 
-    score = roa + profit_margin - debt_ratio - inflation*0.1 - unemployment*0.05
+    prediction = model.predict(data)[0]
 
-    if score > 0:
+    if prediction == 1:
         st.success("Stock appears UNDERVALUED")
     else:
-        st.error("Stock appears OVERVALUED")
+        st.error("Stock appears OVERVALUUED")
